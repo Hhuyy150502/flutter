8aa42f09c109fbda1936ae6ab4652719549affef
Đoàn Quang Huy <96615568+Hhuyy150502@users.noreply.github.com>
2023-09-19 20:33:23 +0700

    Add files via upload

---
+++ flutter/src/main/java/com/example/backendflutter/backendflutterApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendflutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class backendflutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(backendflutterApplication.class, args);
+    }
+
+}
---
+++ flutter/src/main/java/com/example/backendflutter/entity/DestinationEntity.java
@@ -0,0 +1,14 @@
+package com.example.backendflutter.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DestinationEntity {
+    private String destinationName;
+    private String destinationThumbnail;
+    private double destinationRating;
+}
